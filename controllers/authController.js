c2ca0702a04427366e9055ceabb514f312add6f1
saimsaeed <saimsaeed640@gmai.com>
2025-05-25 19:20:14 +0000

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,8 @@ const Login = asyncHandler(async (req, res) => {
   //  set Jwt as an http only cookie
   res.cookie("jwt", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    // secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000 //30days
 
@@ -62,7 +63,7 @@ const Register = asyncHandler(async (req, res) => {
     //  set Jwt as an http only cookie
     res.cookie("jwt", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
+      secure: true,
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000 //30days
   
